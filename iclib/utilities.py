8746e3905f087604c4175ab765f4e879ec5ff132
Juho Kim <juho-kim@outlook.com>
2024-07-15 14:13:13 -0400

    Cleanup code

--- iclib/utilities.py
+++ iclib/utilities.py
@@ -1,6 +1,12 @@
 """This module implements various utilities."""
 
 from collections.abc import Callable
+from collections import deque
+from dataclasses import dataclass, field
+from threading import Event, Lock, Thread
+from time import time
+
+from periphery import GPIO
 
 
 def bit_getter(index: int) -> Callable[[int], bool]:
@@ -31,3 +37,46 @@ def msb_bits_to_byte(*bits: bool) -> int:
         byte |= bit
 
     return byte
+
+
+@dataclass
+class FrequencyMonitor:
+    gpio: GPIO
+    sample_count: int = field(default=5)
+    poll_timeout: float = field(default=1)
+    _lock: Lock = field(init=False, default_factory=Lock)
+    _frequency: float = field(init=False, default=0)
+    _stoppage: Event = field(init=False, default_factory=Event)
+    _thread: Thread = field(init=False)
+
+    def __post_init__(self) -> None:
+        self._thread = Thread(target=self._monitor, daemon=True)
+
+        self._thread.run()
+
+    def _monitor(self) -> None:
+        timestamps = deque[float](maxlen=self.sample_count)
+
+        while not self._stoppage.is_set():
+            if self.gpio.poll(self.poll_timeout):
+                timestamps.append(time())
+
+                if len(timestamps) > 1:
+                    self.frequency = (
+                        len(timestamps)
+                        / (timestamps[-1] - timestamps[0])
+                    )
+
+    @property
+    def frequency(self) -> float:
+        with self._lock:
+            return self._frequency
+
+    @frequency.setter
+    def frequency(self, value: float) -> None:
+        with self._lock:
+            self._frequency = value
+
+    def stop(self) -> None:
+        self._stoppage.set()
+        self._thread.join()
