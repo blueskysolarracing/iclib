328a2b2257f412a408e43aad11b69f55062b62c5
chenghymichael <chenghymichael.acc@gmail.com>
2024-11-25 12:43:47 -0500

    Add GitHub workflow and fix test

--- iclib/tests/test_mcp23s17.py
+++ iclib/tests/test_mcp23s17.py
@@ -102,7 +102,7 @@ class MCP23S17TestCase(TestCase):
 
         mcp23s17.mode = Mode.EIGHT_BIT_MODE
 
-        mock_spi.transfer.assert_has_calls( 
+        mock_spi.transfer.assert_has_calls(
             [
                 call([0b01000001, 0x0A, 0xFF]),
                 call([0b01000000, 0x0A, 0b10000000]),
@@ -111,14 +111,14 @@ class MCP23S17TestCase(TestCase):
         mock_spi.reset_mock()
 
         self.assertEqual(
-            mcp23s17.write(Port.PORTA, Register.INTCON, [0b10101010]),
+            mcp23s17.write_register(Port.PORTA, Register.INTCON, [0b10101010]),
             [0b00000000],
         )
         mock_spi.transfer.assert_called_once_with([0b01000000, 0x04, 0xAA])
         mock_spi.clear_calls()
         mock_spi.reset_mock()
         self.assertEqual(
-            mcp23s17.write(Port.PORTB, Register.INTCON, [0b10101010]),
+            mcp23s17.write_register(Port.PORTB, Register.INTCON, [0b10101010]),
             [0b00000000],
         )
         mock_spi.transfer.assert_called_once_with([0b01000000, 0x14, 0xAA])
