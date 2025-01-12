11d4d5c0d22d1b5eab852aa5ad7e9279a91886f3
alexqin1 <alex.qin@mail.utoronto.ca>
2025-01-11 23:24:48 -0500

    updated bno055 unit tests

--- iclib/tests/test_bno055.py
+++ iclib/tests/test_bno055.py
@@ -41,6 +41,7 @@ class BNO055TestCase(TestCase):
             bno055.write(Register.GRV_DATA_X_LSB, 1),
             [1]
         )
+        #assert called once, and then check arg
         mock_message.assert_called_once_with([Register.GRV_DATA_X_LSB, 1], read=False)
         mock_i2c.transfer.assert_called_once_with(0x28, mock_message)
         mock_i2c.reset_mock()
@@ -58,11 +59,87 @@ class BNO055TestCase(TestCase):
         mock_gpio_out_imu_reset = MagicMock()
         mock_message = MagicMock()
         bno055 = BNO055(mock_i2c, mock_gpio_out_imu_reset)
-
+        bno055.write = MagicMock()
+        
         bno055.select_operation_mode(True, True, True)
         mock_message.assert_called_once_with([Register.OPR_MODE, OperationMode.AMG], read=False)
+
         mock_i2c.transfer.assert_called_once_with(0x28, mock_message)
         mock_i2c.reset_mock()
+
+    def test_quaternion(self) -> None:
+        mock_i2c = MagicMock()
+        mock_gpio_out_imu_reset = MagicMock()
+        mock_message = MagicMock()
+        bno055 = BNO055(mock_i2c, mock_gpio_out_imu_reset)
+        bno055.read = MagicMock()
+
+        bno055.quaternion()
+
+        self.assertEqual(bno055.read.call_count, 8)
+        expected_calls = [
+        call(Register.QUA_DATA_W_MSB, 1),
+        call(Register.QUA_DATA_W_LSB, 1),
+        call(Register.QUA_DATA_X_MSB, 1),
+        call(Register.QUA_DATA_X_LSB, 1),
+        call(Register.QUA_DATA_Y_MSB, 1),
+        call(Register.QUA_DATA_Y_LSB, 1),
+        call(Register.QUA_DATA_Z_MSB, 1),
+        call(Register.QUA_DATA_Z_LSB, 1),
+        ]
+        self.assertEqual(bno055.read.call_args_list, expected_calls)
+        bno055.read.reset_mock()
+
+    def test__get_vector(self) -> None:
+        mock_i2c = MagicMock()
+        mock_gpio_out_imu_reset = MagicMock()
+        mock_message = MagicMock()
+        bno055 = BNO055(mock_i2c, mock_gpio_out_imu_reset)
+
+        bno055.read = MagicMock()
+
+        bno055._get_vector()
+
+        self.assertEqual(bno055.read.call_count, 6)
+
+    def test_magnetic_field(self) -> None:
+        mock_i2c = MagicMock()
+        mock_gpio_out_imu_reset = MagicMock()
+        mock_message = MagicMock()
+        bno055 = BNO055(mock_i2c, mock_gpio_out_imu_reset)
+
+        bno055.read = MagicMock()
+        bno055._get_vector = MagicMock()
+
+        bno055.magnetic_field()
+
+        bno055._get_vector.assert_called_once_with(
+            Register.MAG_DATA_X_MSB,
+            Register.MAG_DATA_X_LSB,
+            Register.MAG_DATA_Y_MSB,
+            Register.MAG_DATA_Y_LSB,
+            Register.MAG_DATA_Z_MSB,
+            Register.MAG_DATA_Z_LSB,
+            bno055.MAGNETIC_FIELD_UNIT_REPRESENTATION,
+        )
+
+        self.assertEqual(bno055.read.call_count, 6)
+        expected_calls = [
+        call(Register.MAG_DATA_X_MSB, 1),
+        call(Register.MAG_DATA_X_LSB, 1),
+        call(Register.MAG_DATA_Y_MSB, 1),
+        call(Register.MAG_DATA_Y_LSB, 1),
+        call(Register.QUA_DATA_Z_MSB, 1),
+        call(Register.QUA_DATA_Z_LSB, 1),
+        ]
+        self.assertEqual(bno055.read.call_args_list, expected_calls)
+
+        bno055._get_vector.reset_mock()
+        bno055.read.reset_mock()
+
+
+
+
 
 
 
