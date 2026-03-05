14f4953d63807b6985308743832c446728293a13
lyn-kate <katelyn.ma128@gmail.com>
2026-03-04 19:43:34 -0500

    Update bno055.py
    Added SA0 pin to control address

--- iclib/bno055.py
+++ iclib/bno055.py
@@ -19,7 +19,7 @@ class Register(IntEnum):
     MAG_ID = 0x02
     """Chip ID of magnetometer Device."""
     GYR_ID = 0x03
-    """Chip ID of gyroscope device."""
+    """Chip ID of gyroscope device."""  
     SW_REV_ID_LSB = 0x04
     """Lower Byte of SW Revision ID."""
     SW_REV_ID_MSB = 0x05
@@ -81,7 +81,7 @@ class Register(IntEnum):
 
 
 class OperationMode(IntEnum):
-    ACCONLY = 0x1
+    ACCONLY = 0x1   
     MAGONLY = 0x2
     GYROONLY = 0x3
     ACCMAG = 0x4
@@ -108,12 +108,12 @@ class Unit(IntEnum):
 
 @dataclass
 class BNO055:
-    ADDRESS: ClassVar[int] = 0x29
     RESET_TIMEOUT: ClassVar[float] = 2.5
     IMU_RESET_GPIO_DIRECTION: ClassVar[str] = 'out'
     IMU_RESET_GPIO_INVERTED: ClassVar[bool] = True
     i2c: I2C
     imu_reset_gpio: GPIO
+    sa0_gpio: GPIO
     _acceleration_unit: Unit = field(init=False, default=Unit.MS2)
     _angular_velocity_unit: Unit = field(init=False, default=Unit.DPS)
     _angle_unit: Unit = field(init=False, default=Unit.DEGREES)
@@ -137,6 +137,11 @@ class BNO055:
             raise ValueError('invalid GPIO direction')
         elif self.imu_reset_gpio.inverted != self.IMU_RESET_GPIO_INVERTED:
             raise ValueError('invalid GPIO inverted status')
+        
+        if self.sa0.read():         # Control address by SA0 Pin, when GPIO is high 0x29
+            self.address = 0x29
+        else:
+            self.address = 0x28
 
     def select_operation_mode(
             self,
@@ -170,13 +175,13 @@ class BNO055:
     def write(self, register: Register, data: int) -> None:
         message = I2C.Message([register, data])
 
-        self.i2c.transfer(self.ADDRESS, [message])
+        self.i2c.transfer(self.address, [message])
 
     def read(self, register: Register, length: int) -> list[int]:
         write_message = I2C.Message([register])
         read_message = I2C.Message([0] * length, read=True)
 
-        self.i2c.transfer(self.ADDRESS, [write_message, read_message])
+        self.i2c.transfer(self.address, [write_message, read_message])
 
         return list(read_message.data)
 
